36bbcea975c682b8af65b617ffb021cfa496d257
Huy Pham <huy.pham@objectbrains.com>
2018-11-24 13:06:45 -0800

    Huy: Add Firebase Authentication by phone. Fix css issue for Android.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,23 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterExtensions } from 'nativescript-angular/router';
+import * as firebase from 'nativescript-plugin-firebase';
 
 @Component({
   selector: 'ns-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css', './login.css'],
   moduleId: module.id,
 })
 export class LoginComponent implements OnInit {
+  isLoggingIn: boolean;
 
   constructor(
     private router: RouterExtensions
   ) { }
 
   ngOnInit() {
+    this.isLoggingIn = false;
   }
 
   logIn() {
-    this.router.navigate(['/home'], {clearHistory: true});
+    this.isLoggingIn = true;
+    firebase.login({
+        type: firebase.LoginType.PHONE,
+        phoneOptions: {
+          phoneNumber: '+19495551234',
+          verificationPrompt: "The received verification code" // default "Verification code"
+        }
+      }).then(
+        result => {
+          this.isLoggingIn = false;
+          console.log('successfully log in\n', result);
+          this.router.navigate(['/home'], { clearHistory: true });
+        },
+        err => {
+          this.isLoggingIn = false;
+          console.log('error =', err);
+        }
+      )
   }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAa349WNHegMsRQ6rWmGoR6NN_b-UOeXJc",
+    authDomain: "sti-mobile-app-test.firebaseapp.com",
+    databaseURL: "https://sti-mobile-app-test.firebaseio.com",
+    projectId: "sti-mobile-app-test",
+    storageBucket: "sti-mobile-app-test.appspot.com",
+    messagingSenderId: "422739635229"
+  }
+}
